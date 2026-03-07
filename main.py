22a4fb828750c32120c730456c3ee56006a97d3d
Valentinka <mborisova.2015@mail.ru>
2026-03-07 11:30:08 +0300

    Masha

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import pygame
 
 
+class Sprite:
+    def __init__(self, center, image):
+        self.image = image
+        self.rect = self.image.get_frect()
+        self.rect.center = center
+
+    def render(self, surface):
+        surface.blit(self, image, self.rect)
+
+class MoveSprite(Sprite):
+    def __init__(self, center, image, speed, direction):
+        super().__init__(center, image)
+
+        self.speed = speed
+        self.direction = direction.normalize()
+
+    def update(self):
+        vector = self.direction * self.speed
+        self.rect.move_ip(vector)
+
+
+
 WINDOW_SIZE = (800, 600)
 MAX_FPS = 60
 
@@ -8,6 +30,11 @@ window = pygame.Window('Towel Defense', WINDOW_SIZE)
 surface = window.get_surface()
 
 clock = pygame.Clock()
+
+center = WINDOW_SIZE[0]/2, WINDOW_SIZE[1]/2
+image = pygame.Surface((50, 50))
+image.fill('red')
+player = Sprite(center, image)
 
 running = True
 
@@ -21,7 +48,7 @@ while running:
 
     #
     surface.fill('white')
-    ...
+    player.render(surface)
     window.flip()
 
     clock.tick(MAX_FPS)
